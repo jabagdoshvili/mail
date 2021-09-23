b149ff3efa0ac5a14bb235d671831931d7fac057
Jaba Bagdoshvili <jabagoshvili@gmail.com>
2021-09-23 18:42:53 +0400

    ///

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,7 @@ $.fn.searchGridData = function (text) {
 }
 
 
-const response = [
-    {
+const response = [{
         date: '06 Mar, 2021',
         email: 'johnySmith@gmail.com',
         sender: 'Johny Smith Public',
@@ -126,8 +125,7 @@ const response = [
 
 $('#rounded-table').InitTable({
     data: response,
-    columns: [
-        {
+    columns: [{
             title: 'Date',
             data: 'date',
         },
@@ -185,7 +183,8 @@ $('#rounded-table').InitTable({
 })
 
 
-void function InitDomEvents() {
+void
+function InitDomEvents() {
 
     $('.status-tab').click(function () {
         $('.filter-dropdown').toggleClass('active')
@@ -196,27 +195,27 @@ void function InitDomEvents() {
         $('.filter-dropdown-header').toggleClass('active')
     })
 
-    $('input[name="search"]').on('input', function() {
+    $('input[name="search"]').on('input', function () {
         $('#rounded-table').searchGridData($(this).val())
     })
 
-    $('.add-button').on('click', function() {
+    $('.add-button').on('click', function () {
         $('.email-popup').addClass('visible')
     })
 
-    $('.set-up').on('click', function() {
+    $('.set-up').on('click', function () {
         $('.email-popup').removeClass('visible')
         $('.add-inbox').addClass('visible')
     })
 
-    $('.calendar-tab').on('click', function() {
+    $('.calendar-tab').on('click', function () {
         $('.calendar-wrapper ').toggleClass('visible')
         $('.filter-dropdown').removeClass('visible')
     })
 
-    $(document).on('click', 'tbody tr > td:not(:last-child)', function(e) {
-        var target = $( e.target );
-        if(target.is('.round')) {
+    $(document).on('click', 'tbody tr > td:not(:last-child)', function (e) {
+        var target = $(e.target);
+        if (target.is('.round')) {
             $('.mail-inside').removeClass('visible')
         } else {
             $('.mail-inside').addClass('visible')
@@ -224,7 +223,7 @@ void function InitDomEvents() {
     })
 
 
-    $('.round input[type="checkbox"]').on('click', function() {
+    $('.round input[type="checkbox"]').on('click', function () {
         if ($(this).is(':checked')) {
             $('.hidden').addClass('visible')
         } else {
@@ -232,69 +231,67 @@ void function InitDomEvents() {
         }
     })
 
-    $('.close').on('click', function() {
+    $('.close').on('click', function () {
         $('.overflow').removeClass('visible')
     })
 
-    $('.modal-menu ul li').on('click', function() {
+    $('.modal-menu ul li').on('click', function () {
         let atr = $(this).attr('tab')
         $(this).addClass('active').siblings().removeClass('active')
         let percent = $(this).index() * 100
-        $(`.${atr}`,'.modal-wrapper').addClass('active').siblings().removeClass('active')
-        if(atr == 'schedule' && $(`.${atr}`,'.modal-wrapper').hasClass('active')) {
+        $(`.${atr}`, '.modal-wrapper').addClass('active').siblings().removeClass('active')
+        if (atr == 'schedule' && $(`.${atr}`, '.modal-wrapper').hasClass('active')) {
             $('.send').html('Save')
         } else {
             $('.send').html('Proceed to Save')
         }
     })
 
-    $('form').on('submit', function(ev) {
+    $('form').on('submit', function (ev) {
         ev.preventDefault()
-        if(!$(this).hasClass('schedule')) {
+        if (!$(this).hasClass('schedule')) {
             $('.modal-menu li.active').next().trigger('click')
         } else {
             // submit here
         }
     })
 
-    $(document).on('click', function() {
+    $(document).on('click', function () {
         $('.filter-dropdown-header.active').removeClass('active')
     })
 
-    $('.filter-dropdown-header, .dropdown').on('click', function(e) {
+    $('.filter-dropdown-header, .dropdown').on('click', function (e) {
         e.stopPropagation()
     })
 
-    $(document).on('click', function() {
+    $(document).on('click', function () {
         $('.filter-dropdown.active').removeClass('active')
     })
 
-    $('.filter-dropdown, .status-tab').on('click', function(e) {
+    $('.filter-dropdown, .status-tab').on('click', function (e) {
         e.stopPropagation()
     })
 
-    $(document).on('click', function() {
+    $(document).on('click', function () {
         $('.overflow:not(.mail-inside).visible').removeClass('visible')
     })
 
-    $('.modals, .add-button').on('click', function(e) {
+    $('.modals, .add-button').on('click', function (e) {
         e.stopPropagation()
     })
 
-    $('body').on('click', function(e) {
-        if($('.mail-inside').hasClass('visible')) {
+    $('body').on('click', function (e) {
+        if ($('.mail-inside').hasClass('visible')) {
             $('.mail-inside').removeClass('visible')
         }
     })
 
-    $(document).on('click', function() {
+    $(document).on('click', function () {
         $('.calendar-wrapper.visible').removeClass('visible')
     })
 
-    $('.calendar-wrapper, .calendar-tab').on('click', function(e) {
+    $('.calendar-wrapper, .calendar-tab').on('click', function (e) {
         e.stopPropagation()
     })
 
 }()
-
-  
